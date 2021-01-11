bc72c2635c2573be5b9011f7dcc624bc22c5f502
anastasia-okromeshko <nastya_yankovich@mail.ru>
2021-01-11 15:43:36 +0200

    zadanie_12

---
+++ zadanie_12.py
@@ -0,0 +1,17 @@
+# Дан словарь: {'test': 'test_value', 'europe': 'eur', 'dollar':
+# 'usd', 'ruble': 'rub'}
+# Добавить каждому ключу число равное длине этого
+# ключа (пример {‘key’: ‘value’} -> {‘key3’: ‘value’}). Чтобы
+# получить список ключе - использовать метод .keys()
+slovar = {'test': 'test_value', 'europe': 'eur', 'dollar': 'usd', 'ruble': 'rub'}
+
+keys = list(slovar.keys())
+
+i = 0
+len_keys = len(keys)
+
+while i < len_keys:
+    new_key = f'{keys[i]}{len(keys[i])}'
+    slovar[new_key] = slovar.pop(keys[i])
+    i += 1
+print(slovar)
